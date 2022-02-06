5560e27edf780786a3ca0e8911483d2ab53c8337
Abrar <abrar.ali75@gmail.com>
2022-02-06 05:14:38 -0800

    changes text in app

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>devops boiiiiii</p>
+        <p>GGWP</p>
         <a
           className="App-link"
           href="https://reactjs.org"
